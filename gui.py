5aa1c34e88893f005107da72a68e044e021d301e
Chewycide <gitchewycide@gmail.com>
2022-08-18 10:05:27 +0800

    Add file menu with exit action

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QTextEdit, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QGridLayout, QTextEdit, QPushButton, QMenu, QAction, qApp
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
 
     def __init__(self):
 
@@ -15,7 +15,16 @@ class MainWindow(QWidget):
     def InitWin(self):
         
         self.setGeometry(700, 100, 500, 700)
+        self.setWindowTitle("Simple Text Editor by Chewycide")
+
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu("File")
+
+        exit_act = QAction("Exit", self)
+        exit_act.triggered.connect(qApp.quit)
+        file_menu.addAction(exit_act)
 
 
     def InitUI(self):
+        
         pass
