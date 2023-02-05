cd27b6a80a7e2be4ca0350a2584f4ed8562063ab
Chewycide <gitchewycide@gmail.com>
2023-02-05 10:39:48 +0800

    Refactor Code

--- app/window.py
+++ app/window.py
@@ -1,20 +1,15 @@
-from distutils import text_file
 from PyQt5.QtWidgets import (
     QMainWindow,
     QVBoxLayout,
-    QGridLayout,
     QHBoxLayout,
-    QPlainTextEdit,
+    QTextEdit,
     QPushButton,
-    QMenu,
     QAction,
     QWidget,
     QFileDialog,
     QMessageBox,
     qApp
     )
-from PyQt5.QtCore import Qt
-from app.widgets import CustomButton
 
 
 class MainWindow(QMainWindow):
@@ -25,14 +20,19 @@ class MainWindow(QMainWindow):
 
         self.fname = None
 
+        self.InitStyle()
         self.InitWindow()
-        self.InitWidgets()
+        self.InitUI()
         self.show()
 
 
+    def InitStyle(self):
+        with open("app/styles/ui.qss", "r") as stylesheet:
+            self.setStyleSheet(stylesheet.read())
+
+
     def InitWindow(self):
-        """method to initialize the window"""
-        
+        """Initialize window properties."""
         self.setGeometry(700, 100, 500, 700)
         self.setMinimumSize(500, 700)
         self.setMaximumSize(1000, 900)
@@ -44,7 +44,7 @@ class MainWindow(QMainWindow):
 
         open_act = QAction("Open", self)
         open_act.setStatusTip("Open a File.")
-        open_act.triggered.connect(self.browsefiles)
+        open_act.triggered.connect(self.browse_files)
         file_menu.addAction(open_act)
 
         exit_act = QAction("Exit", self)
@@ -53,46 +53,41 @@ class MainWindow(QMainWindow):
         file_menu.addAction(exit_act)
 
 
-    def InitWidgets(self):
-        """method to initialize widgets and layouts"""
+    def InitUI(self):
+        """Initialize Layouts and Widgets"""
 
-# ----- Widgets
-
+        # --- center widget
         widget = QWidget(self)
         self.setCentralWidget(widget)
+        main_layout = QVBoxLayout()
 
-        self.text_edit = QPlainTextEdit()
+        # --- row 1
+        self.text_edit = QTextEdit()
+        main_layout.addWidget(self.text_edit)
         
-        self.save_button = CustomButton("Save", self)
+        
+        # --- row 2
+        row_2 = QHBoxLayout()
+
+        self.save_button = QPushButton("Save", self)
         self.save_button.setStatusTip("Save current file")
         if self.fname == None:
             self.save_button.setEnabled(False)
-        self.save_button.clicked.connect(self.savefile)
+        self.save_button.clicked.connect(self.save_file)
 
-        self.saveas_button = CustomButton("Save as", self)
+        self.saveas_button = QPushButton("Save as", self)
         self.saveas_button.setStatusTip("Save to a directory")
-        self.saveas_button.clicked.connect(self.savetofile)
+        self.saveas_button.clicked.connect(self.save_to_file)
+        
+        row_2.addWidget(self.save_button)
+        row_2.addWidget(self.saveas_button)
+        main_layout.addLayout(row_2)
 
-# ----- Stylesheet import
-
-        with open("styles/ui.qss", "r") as stylesheet:
-            self.setStyleSheet(stylesheet.read())
-
-# ----- Layouts
-
-        hbox = QHBoxLayout()
-        hbox.addWidget(self.save_button)
-        hbox.addWidget(self.saveas_button)
-
-        main_grid = QGridLayout()
-        main_grid.setSpacing(10)
-        main_grid.addWidget(self.text_edit, 0, 0, 1, 3)
-        main_grid.addLayout(hbox, 1, 1)
-
-        widget.setLayout(main_grid)
+        # --- main layout setup
+        widget.setLayout(main_layout)
 
 
-    def browsefiles(self):
+    def browse_files(self):
         """method to browse and open a text file to text editor using file explorer"""
         
         self.fname = QFileDialog.getOpenFileName(self, "Open a text file", "C:/", "Text files (*.txt)")
@@ -106,7 +101,7 @@ class MainWindow(QMainWindow):
             return None
 
 
-    def savefile(self):
+    def save_file(self):
         """method to write to opened file"""
 
         try:
@@ -131,10 +126,10 @@ class MainWindow(QMainWindow):
             self.setStatusTip("Error: No filepath to save to.")
 
 
-    def savetofile(self):
+    def save_to_file(self):
         """method to save current text to a specified file path"""
 
-        self.fname = QFileDialog.getSaveFileName(self, "Save as", "C:/", "Text files (*.txt)")
+        self.fname = QFileDialog.getSave_FileName(self, "Save as", "C:/", "Text files (*.txt)")
         with open(f"{self.fname[0]}", "w") as text_file:
             text = self.text_edit.toPlainText()
             text_file.write(text)
